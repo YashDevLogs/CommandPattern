bda053015e89343ee3a62244e26af00e5769be45
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-02-23 19:13:55 +0530

    Created Abstract Commands
    Created an interface called ICommand as the parent abstract command. Declare an Execute() method in the interface.
    Created another abstract Command called UnitCommand which inherits ICommand interface. It should store all information related to a unit's command.

---
+++ Assets/Scripts/Commands/Abstract Commands/ICommand.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICommand
+{
+    // This method defines the contract for executing a command.
+    void Execute();
+}
+
